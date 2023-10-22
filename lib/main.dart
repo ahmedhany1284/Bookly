7a0ea0d22d99aee2da038958434dc2511e65213c
ahmedhany1284 <ahmedhany4900@gmail.com>
2023-10-23 01:11:30 +0300

    create splashView and the the partioning of the project

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:bookly/features/splash/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'constants/constants.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kprimaryColor,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
+
