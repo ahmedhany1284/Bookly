7d2b54efae706f239e66b2268164a3794c3ce497
ahmedhany1284 <ahmedhany4900@gmail.com>
2023-10-30 22:54:51 +0200

    implement similar books

--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -44,4 +44,22 @@ class HomeRepoImp extends HomeRepo {
       return left(ServerFailure(e.toString()));
     }
   }
+
+
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({required String category}) async {
+    try {
+      var data = await apiService.get(
+          endpoint: 'volumes?Filtering=free-ebooks&Sorting=relevance &q=computer science');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
 }
